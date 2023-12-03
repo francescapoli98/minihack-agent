c9645df152ce351a236b4a28c816eeaca87531ae
Daniele Bedini <108469620+danielebedini@users.noreply.github.com>
2023-12-03 13:53:36 +0100

    Added ad improved  version of the heuristic

--- heuristic_FD.py
+++ heuristic_FD.py
@@ -4,7 +4,7 @@ from utils import *
 # This heuristic gives priority to the weapons first, then to the monsters
 # The agent will try to pick up the weapons first, then it will try to kill the monsters
 # The agent will try to reach the exit only if there are no weapons and no monsters in the room
-def heuristic_fd(game_map: np.ndarray, current_position: Tuple[int, int], end_target: Tuple[int, int], hp_player: int):
+def old_heuristic(game_map: np.ndarray, current_position: Tuple[int, int], end_target: Tuple[int, int], hp_player: int):
     
     coord_weapons = get_weapon_location(game_map) #get the coordinates of the weapons
     coord_monsters = get_monster_location(game_map) #get the coordinates of the monsters
@@ -26,18 +26,65 @@ def heuristic_fd(game_map: np.ndarray, current_position: Tuple[int, int], end_ta
     score = euclidean_distance(current_position, target) #score is the euclidean distance between the current position and the target
     return score
 
-'''
-def get_best_move_euclidean(game_map: np.ndarray, 
-                  current_position: Tuple[int, int],
-                  end_target: Tuple[int, int]
-                 ) -> Tuple[int, int]: 
-    moves = get_valid_moves(game_map,current_position)
-    min = float('inf')
-    coord = (0,0)
-    for move in moves:
-        md = euclidean_distance(move, end_target)
-        if md < min:
-            min = md
-            coord = move
-    return coord
-'''
+def heuristic_fd(game_map, move, end_target, hp):
+    """
+    Calcola un'euristica personalizzata tenendo conto della sequenza di azioni: raccogliere l'arma, uccidere i mostri e raggiungere l'uscita.
+
+    Args:
+    - game_map: Lista di tuple rappresentante la griglia con informazioni su mostri, armi, giocatore e uscita.
+    - move: Tupla (x, y) rappresentante le coordinate della prossima mossa.
+    - goal: Tupla (x, y) rappresentante le coordinate dell'obiettivo (uscita).
+
+    Returns:
+    - int: Valore dell'euristica come intero.
+    """
+    # get the coordinates of the weapons and the monsters
+    weapon_location = get_weapon_location(game_map) 
+    monster_location = get_monster_location(game_map) 
+
+    # weights: the higher the weight, the higher the priority, we can tune them as we want
+    weapon_weight = 10
+    monster_weight = 5
+    exit_weight = 1
+
+    inf = 999999999 # infinity
+
+    # distance from the exit
+    target_distance = manhattan_distance(move, end_target)
+
+    # distance from the weapon, the agents will try to pick up the nearest weapon
+    # weapon_distance = manhattan_distance(move, weapon_location[0]) if weapon_location else inf
+    min_weapon = 0
+    if weapon_location: #if there are weapons in the room
+        for weapon in weapon_location:  
+            if manhattan_distance(move, weapon) < min_weapon:
+                min_weapon = manhattan_distance(move, weapon)
+                weapon_distance = min_weapon
+            else:
+                weapon_distance = manhattan_distance(move, weapon)
+    else: #if there are no weapons in the room
+        weapon_distance = inf
+
+    # distance from the monster, the agents will try to kill the nearest monster first
+    min_monster = 0
+    if monster_location: #if there are monsters in the room
+        for monster in monster_location: #for each monster in the room
+            if manhattan_distance(move, monster) < min_monster: 
+                min_monster = manhattan_distance(move, monster) 
+                monster_distance = min_monster
+            else:
+                monster_distance = manhattan_distance(move, monster)
+    else: #if there are no monsters in the room
+        monster_distance = inf
+
+    # heuristic weighted sum of the distances
+    weighted_heuristic = (
+        weapon_weight * weapon_distance +
+        monster_weight * monster_distance +
+        exit_weight * target_distance 
+    )
+
+    # we can scale the heuristic as we want
+    #scaled_heuristic = int(weighted_heuristic * 0.1)  # 0.1 is the scale factor
+
+    return int(weighted_heuristic) 
